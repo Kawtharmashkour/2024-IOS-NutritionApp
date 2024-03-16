c664ce4449dd76f2eb2be4519138f02ae5942f2d
Kawtharmashkour <kawtharmashkour@yahoo.com>
2024-03-16 00:11:09 -0400

    Add server and view folder with sample file in each

---
+++ NutriScan/NutriScan/server/server.swift
@@ -0,0 +1,8 @@
+//
+//  server.swift
+//  NutriScan
+//
+//  Created by KAWTHAR on 2024-03-16.
+//
+
+import Foundation
---
+++ NutriScan/NutriScan/views/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  NutriScan
+//
+//  Created by KAWTHAR on 2024-03-16.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
