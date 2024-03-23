29eb25a96a826982a315cbdba30ada73e7d8e3f8
Kawtharmashkour <kawtharmashkour@yahoo.com>
2024-03-23 11:36:53 -0400

    RecipeListViewModel and solve corrupted Package.resolved

--- NutriScan/NutriScan/View Models/RecipeListViewModel.swift
+++ NutriScan/NutriScan/View Models/RecipeListViewModel.swift
@@ -7,12 +7,13 @@
 
 import Foundation
 
+//@MainActor: is the every func and properties will be automatically called on main thread
 @MainActor
 class RecipeListViewModel: ObservableObject {
     
-    @Published var recipeList: [RecipeListViewModel] = []
+    @Published var recipeList: [RecipeViewModel] = []
     
-    func populateRecipeList() async {
+    func populateRecipeList() async -> Void {
         
         do {
             let recipeListResponse = try await WebService().get(url: Constants.Urls.searchRecipeURL) { data in
@@ -21,7 +22,10 @@ class RecipeListViewModel: ObservableObject {
                     //try?: If decoding succeeds, the result will be an optional containing the decoded value. If decoding fails (due to an error being thrown), the result will be nil. because in Webservice if result of parse is nil will thrown an error
                     return try? JSONDecoder().decode(RecipeListResponse.self, from: data)
                     }
-           // self.recipeList = recipeListResponse.hits.map(RecipeViewModel.init)
+            
+            // Map the recipes from RecipeListResponse to RecipeViewModel
+            // as soon as using @Published we should use @MainActor : so the result of self.recipeList will be (on the main queue on the main thread). Note: @MainActor is instead of dispatch
+            self.recipeList = recipeListResponse.hits.map(RecipeViewModel.init)
             
         } catch {
             print(error)
