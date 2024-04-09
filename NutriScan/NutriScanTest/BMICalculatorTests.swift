d63befea53684ce31db5389fd6764e2b59fe4449
Behnaz <behnazkhalili84@gmail.com>
2024-04-09 12:44:27 -0400

    Unit Test

---
+++ NutriScan/NutriScanTest/BMICalculatorTests.swift
@@ -0,0 +1,14 @@
+//
+//  BMICalculatorTests.swift
+//  NutriScanTest
+//
+//  Created by behnaz Khalili on 2024-04-07.
+//
+
+import XCTest
+@testable import NutriScan
+final class BMICalculatorTests: XCTestCase {
+
+    
+
+}
